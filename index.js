47d0070d6614cb3958b73aa54eae948aeac53292
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-07-03 18:16:39 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./routes/router.js"
-// import orderRouter from "./routes/orderRoute.js"
 
 const port = 4500;
 const app = express();
@@ -23,7 +22,6 @@ mongoDB()
 
 
 app.use("/auth",authRouter)
-// app.use("/order",orderRouter)
 
 app.get(("/", (req,res)=>{
     res.send("working")
